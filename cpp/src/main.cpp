1bcc5e70b9c4b86a6f89dfc56d520031135d690a
Shreyas Raorane <raorane@seas.upenn.edu>
2025-11-21 21:47:35 -0500

    new scenarios added and custom file stuff added

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -6,10 +6,17 @@ using namespace std;
 using namespace Eigen;
 using namespace std::chrono;
 
-int main() {
+int main(int argc, char** argv) {
 	cout << "Solving multi-robot motion planning problem..." << endl;
 
-    std::ifstream my_config_file("../config/config.json");
+    // default config file
+    std::string config_path = "../config/config.json";
+
+    if (argc > 1) {
+        config_path = argv[1];
+    }
+
+    std::ifstream my_config_file(config_path);
     assert(my_config_file && "Couldn't find the config file");
 
     Simulator sim(my_config_file);
